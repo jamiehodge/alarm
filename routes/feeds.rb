2b6c74c88e3a3cf978226d46bca2298b3bf1ca3a
Jamie Hodge <jamiehodge@me.com>
2011-03-30 16:04:32 +0200

    feed titles

--- routes/feeds.rb
+++ routes/feeds.rb
@@ -3,10 +3,11 @@ class App < Sinatra::Base
 	get '/feeds/:id' do
 		feed = open_resource("#{settings.pcp['library']}/atom_feeds/#{params[:id]}")
 		etag feed.meta['etag'].gsub(/"/, '') unless flash.has?(:notice) || flash.has?(:error)
-		@title = "#{settings.site['title']}: #{feed.title}"
+		parsed_feed = Atom::Feed.parse(feed)
+		@title = "#{settings.site['title']}: #{parsed_feed.title}"
 		haml :'feeds/show', 
 			:layout => :'layouts/app', 
-			:locals => { :feed => Atom::Feed.parse(feed)}
+			:locals => { :feed => parsed_feed}
 	end
 
 	put '/feeds/:id' do
@@ -18,11 +19,12 @@ class App < Sinatra::Base
 		authenticate!
 		feed = open_resource("#{settings.pcp['library']}/atom_feeds/#{params[:id]}")
 		etag feed.meta['etag'].gsub(/"/, '') unless flash.has?(:notice) || flash.has?(:error)
-		@title = "#{settings.site['title']}: #{feed.title}"
+		parsed_feed = Atom::Feed.parse(feed)
+		@title = "#{settings.site['title']}: #{parsed_feed.title}"
 		session[:return_to] = request.path_info
 		haml :'feeds/edit',
 		:layout => :'layouts/app',
-		:locals => { :feed => Atom::Feed.parse(feed)}
+		:locals => { :feed => parsed_feed}
 	end
 
 	put '/feeds/:id/image' do
@@ -34,30 +36,33 @@ class App < Sinatra::Base
 	get '/keywords/:id' do
 		feed = open_resource("#{settings.pcp['library']}/keyword_atom_feeds/#{URI.encode(params[:id])}")
 		etag feed.meta['etag'].gsub(/"/, '') unless flash.has?(:notice) || flash.has?(:error)
-		@title = "#{settings.site['title']}: #{feed.title}"
+		parsed_feed = Atom::Feed.parse(feed)
+		@title = "#{settings.site['title']}: #{parsed_feed.title}"
 		haml :'feeds/show',
 			:layout => :'layouts/app',
-			:locals => { :feed => Atom::Feed.parse(feed)}
+			:locals => { :feed => parsed_feed}
 	end
 
 	get '/users/:id' do
 		feed = open_resource("#{settings.pcp['library']}/user_atom_feeds/#{URI.encode(params[:id])}")
 		etag feed.meta['etag'].gsub(/"/, '') unless flash.has?(:notice) || flash.has?(:error)
-		@title = "#{settings.site['title']}: #{feed.title}"
+		parsed_feed = Atom::Feed.parse(feed)
+		@title = "#{settings.site['title']}: #{parsed_feed.title}"
 		haml :'feeds/show',
 		:layout => :'layouts/app',
-		:locals => { :feed => Atom::Feed.parse(feed)}
+		:locals => { :feed => parsed_feed}
 	end
 
 	get '/users/:id/edit' do
 		authenticate!
 		feed = open_resource("#{settings.pcp['library']}/user_atom_feeds/#{params[:id]}")
 		etag feed.meta['etag'].gsub(/"/, '') unless flash.has?(:notice) || flash.has?(:error)
-		@title = "#{settings.site['title']}: #{feed.title}"
+		parsed_feed = Atom::Feed.parse(feed)
+		@title = "#{settings.site['title']}: #{parsed_feed.title}"
 		session[:return_to] = "/users/#{params[:id]}/edit"
 		haml :'feeds/edit',
 		:layout => :'layouts/app',
-		:locals => { :feed => Atom::Feed.parse(feed)}
+		:locals => { :feed => parsed_feed}
 	end
 	
 end
